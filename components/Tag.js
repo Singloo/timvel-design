e9b576761c67ad6bbd1ada421642d6f8a4767b75
Singloo <singloo.oo.yang@gmail.com>
2018-09-03 22:59:21 +0800

    9.3

--- components/Tag.js
+++ components/Tag.js
@@ -7,11 +7,33 @@ import { base } from '../../js/utils';
 const { colors } = base;
 class Tag extends Component {
   render() {
-    const { title, onPress, style, textStyle } = this.props;
+    const { title, onPress, style, textStyle, popularity } = this.props;
+    let textColor = colors.depGrey;
+    let fontSize = 18;
+    if (typeof popularity !== 'undefined') {
+      if (popularity > 0 && popularity < 10) {
+        textColor = colors.lime;
+        fontSize = 22;
+      } else if (popularity >= 10 && popularity < 40) {
+        textColor = colors.purple;
+        fontSize = 26;
+      } else if (popularity >= 40 && popularity < 80) {
+        textColor = colors.teal;
+        fontSize = 30;
+      } else if (popularity >= 80 && popularity < 200) {
+        textColor = colors.yellow;
+        fontSize = 34;
+      } else if (popularity >= 400) {
+        textColor = colors.red;
+        fontSize = 40;
+      }
+    }
     return (
       <Touchable onPress={onPress && onPress}>
-        <View style={[styles.container, style]}>
-          <Text style={[textStyle]}>{title}</Text>
+        <View style={[styles.container, style, { borderColor: textColor }]}>
+          <Text style={[textStyle, { color: textColor, fontSize: fontSize }]}>
+            {title}
+          </Text>
         </View>
       </Touchable>
     );
