14e9e577c2471d7bf1a7ec24186bd6e041270c38
Singloo <singloo.oo.yang@gmail.com>
2019-03-30 10:26:50 +0800

    fix bug in movable item

--- HOCs/createMoveableComponent.js
+++ HOCs/createMoveableComponent.js
@@ -10,92 +10,92 @@ import {
   startWith,
   filter,
 } from 'rxjs/operators';
-const getLastestTwoValue = arr => [arr[arr.length - 2], arr[arr.length - 1]];
-const generatePointObj = (left, top) => ({ left, top });
-const getBoundays = (edge, itemSize) => ({
-  TOP_BOUNDARY: edge,
-  LEFT_BOUNDARY: edge,
-  BOTTOM_BOUNDARY: SCREEN_HEIGHT - edge - itemSize,
-  RIGHT_BOUNDARY: SCREEN_WIDTH - edge - itemSize,
-});
-const validateDiff = diff => {
-  return diff < 0 ? Math.max(diff, -4) : Math.min(4, diff);
-};
-const generateArr = (diff, distance, start, line) => {
-  return new Array(Math.abs(Math.floor(distance / diff)))
-    .fill(diff)
-    .concat(distance % diff)
-    .map((x, index) => index * diff + x + start)
-    .map(x => line(x));
-};
-const getBoundaryOfLine = (
-  diff,
-  b,
-  { TOP_BOUNDARY, LEFT_BOUNDARY, BOTTOM_BOUNDARY, RIGHT_BOUNDARY },
-) => {
-  if (diff < 0 && b <= TOP_BOUNDARY) {
-    return {
-      target: 'top',
-      boundary: TOP_BOUNDARY,
-    };
-  }
-  if (diff > 0 && b >= BOTTOM_BOUNDARY) {
-    return {
-      target: 'top',
-      boundary: BOTTOM_BOUNDARY,
-    };
-  }
-  return {
-    target: 'left',
-    boundary: diff < 0 ? LEFT_BOUNDARY : RIGHT_BOUNDARY,
-  };
-};
-const calLine = (point1, point2, { edge, itemSize }) => {
-  const k = point1.left - point2.left / (point1.top - point2.top);
-  const b = point1.top - k * point1.left;
-  const boundarys = ({
-    TOP_BOUNDARY,
-    LEFT_BOUNDARY,
-    BOTTOM_BOUNDARY,
-    RIGHT_BOUNDARY,
-  } = getBoundays(edge, itemSize));
-  let line;
-  let diff;
-  let target;
-  let boundary;
-  if (point1.left === point2.left) {
-    line = (y = null) => {
-      // x is constant
-      return generatePointObj(point1.left, y);
-    };
-    diff = validateDiff(point2.top - point1.top);
-    target = 'top';
-    boundary = diff <= 0 ? TOP_BOUNDARY : BOTTOM_BOUNDARY;
-    return { line, diff, target, boundary };
-  }
-  if (point1.top === point2.top) {
-    line = (x = null) => {
-      //y is constant
-      return generatePointObj(x, point1.top);
-    };
-    diff = validateDiff(point2.left - point1.left);
-    target = 'left';
-    boundary = diff <= 0 ? LEFT_BOUNDARY : RIGHT_BOUNDARY;
-    return { line, diff, target, boundary };
-  }
+// const getLastestTwoValue = arr => [arr[arr.length - 2], arr[arr.length - 1]];
+// const generatePointObj = (left, top) => ({ left, top });
+// const getBoundays = (edge, itemSize) => ({
+//   TOP_BOUNDARY: edge,
+//   LEFT_BOUNDARY: edge,
+//   BOTTOM_BOUNDARY: SCREEN_HEIGHT - edge - itemSize,
+//   RIGHT_BOUNDARY: SCREEN_WIDTH - edge - itemSize,
+// });
+// const validateDiff = diff => {
+//   return diff < 0 ? Math.max(diff, -4) : Math.min(4, diff);
+// };
+// const generateArr = (diff, distance, start, line) => {
+//   return new Array(Math.abs(Math.floor(distance / diff)))
+//     .fill(diff)
+//     .concat(distance % diff)
+//     .map((x, index) => index * diff + x + start)
+//     .map(x => line(x));
+// };
+// const getBoundaryOfLine = (
+//   diff,
+//   b,
+//   { TOP_BOUNDARY, LEFT_BOUNDARY, BOTTOM_BOUNDARY, RIGHT_BOUNDARY },
+// ) => {
+//   if (diff < 0 && b <= TOP_BOUNDARY) {
+//     return {
+//       target: 'top',
+//       boundary: TOP_BOUNDARY,
+//     };
+//   }
+//   if (diff > 0 && b >= BOTTOM_BOUNDARY) {
+//     return {
+//       target: 'top',
+//       boundary: BOTTOM_BOUNDARY,
+//     };
+//   }
+//   return {
+//     target: 'left',
+//     boundary: diff < 0 ? LEFT_BOUNDARY : RIGHT_BOUNDARY,
+//   };
+// };
+// const calLine = (point1, point2, { edge, itemSize }) => {
+//   const k = point1.left - point2.left / (point1.top - point2.top);
+//   const b = point1.top - k * point1.left;
+//   const boundarys = ({
+//     TOP_BOUNDARY,
+//     LEFT_BOUNDARY,
+//     BOTTOM_BOUNDARY,
+//     RIGHT_BOUNDARY,
+//   } = getBoundays(edge, itemSize));
+//   let line;
+//   let diff;
+//   let target;
+//   let boundary;
+//   if (point1.left === point2.left) {
+//     line = (y = null) => {
+//       // x is constant
+//       return generatePointObj(point1.left, y);
+//     };
+//     diff = validateDiff(point2.top - point1.top);
+//     target = 'top';
+//     boundary = diff <= 0 ? TOP_BOUNDARY : BOTTOM_BOUNDARY;
+//     return { line, diff, target, boundary };
+//   }
+//   if (point1.top === point2.top) {
+//     line = (x = null) => {
+//       //y is constant
+//       return generatePointObj(x, point1.top);
+//     };
+//     diff = validateDiff(point2.left - point1.left);
+//     target = 'left';
+//     boundary = diff <= 0 ? LEFT_BOUNDARY : RIGHT_BOUNDARY;
+//     return { line, diff, target, boundary };
+//   }
 
-  diff = validateDiff(point2.left - point1.left);
-  const res = getBoundaryOfLine(diff, b, boundarys);
-  line =
-    res.target === 'left'
-      ? (x = null) => {
-          return generatePointObj(x, k * x + b);
-        }
-      : (y = null) => generatePointObj((y - b) / k, y);
-  // target = 'left';
-  // boundary = diff <= 0 ? LEFT_BOUNDARY : RIGHT_BOUNDARY;
-  return { line, diff, ...res };
-};
+//   diff = validateDiff(point2.left - point1.left);
+//   const res = getBoundaryOfLine(diff, b, boundarys);
+//   line =
+//     res.target === 'left'
+//       ? (x = null) => {
+//           return generatePointObj(x, k * x + b);
+//         }
+//       : (y = null) => generatePointObj((y - b) / k, y);
+//   // target = 'left';
+//   // boundary = diff <= 0 ? LEFT_BOUNDARY : RIGHT_BOUNDARY;
+//   return { line, diff, ...res };
+// };
 export default function createMoveableComp(Comp) {
   return class extends React.PureComponent {
     static defaultProps = {
@@ -145,24 +145,25 @@ export default function createMoveableComp(Comp) {
       if (diff === 0) {
         return;
       }
-      let duration = 250;
-      if (diff < 50) {
-        duration = 100;
-      }
-      if (diff >= 50 && diff < 150) {
-        duration = 150;
-      }
-      if (diff >= 150 && diff < 250) {
-        duration = 200;
-      }
+      // let duration = 250;
+      // if (diff < 50) {
+      //   duration = 100;
+      // }
+      // if (diff >= 50 && diff < 150) {
+      //   duration = 150;
+      // }
+      // if (diff >= 150 && diff < 250) {
+      //   duration = 200;
+      // }
       this.topAnimation && this.topAnimation.stop();
       this.topAnimation = Animated.spring(this.state.top, {
         toValue,
-        duration,
+        // duration,
         easing: Easing.in(Easing.back()),
         useNativeDriver: true,
       });
-      requestAnimationFrame(() => this.topAnimation.start());
+      // requestAnimationFrame(() => this.topAnimation.start());
+      setTimeout(this.topAnimation.start, 100);
       // this.topAnimation.start();
     };
 
@@ -171,42 +172,41 @@ export default function createMoveableComp(Comp) {
       if (diff === 0) {
         return;
       }
-      let duration = 250;
-      if (diff < 50) {
-        duration = 100;
-      }
-      if (diff >= 50 && diff < 150) {
-        duration = 150;
-      }
-      if (diff >= 150 && diff < 250) {
-        duration = 200;
-      }
+      // let duration = 250;
+      // if (diff < 50) {
+      //   duration = 100;
+      // }
+      // if (diff >= 50 && diff < 150) {
+      //   duration = 150;
+      // }
+      // if (diff >= 150 && diff < 250) {
+      //   duration = 200;
+      // }
       this.leftAnimation && this.leftAnimation.stop();
       this.leftAnimation = Animated.spring(this.state.left, {
         toValue,
-        duration,
+        // duration,
         easing: Easing.in(Easing.back()),
         useNativeDriver: true,
       });
-      requestAnimationFrame(() => this.leftAnimation.start());
-      // this.leftAnimation.start();
+      setTimeout(this.leftAnimation.start, 100);
     };
 
-    _predictPath = ({ diff, target, distance, start, line }) => {
-      const arr = generateArr(diff, distance, start, line);
-      console.warn(arr);
-      this.predictPath$ = from(arr)
-        .pipe(
-          concatMap(x =>
-            of(null).pipe(
-              delay(20),
-              startWith(x),
-              filter(v => v !== null),
-            ),
-          ),
-        )
-        .subscribe(this.state$);
-    };
+    // _predictPath = ({ diff, target, distance, start, line }) => {
+    //   const arr = generateArr(diff, distance, start, line);
+    //   console.warn(arr);
+    //   this.predictPath$ = from(arr)
+    //     .pipe(
+    //       concatMap(x =>
+    //         of(null).pipe(
+    //           delay(20),
+    //           startWith(x),
+    //           filter(v => v !== null),
+    //         ),
+    //       ),
+    //     )
+    //     .subscribe(this.state$);
+    // };
     _panResponder = PanResponder.create({
       onStartShouldSetPanResponder: evt => {
         return true;
@@ -251,22 +251,22 @@ export default function createMoveableComp(Comp) {
         });
       },
       onPanResponderRelease: evt => {
-        this._locationX = null;
-        this._locationY = null;
-        const { nativeEvent } = evt;
-        // const { pageX, pageY } = nativeEvent;
-        const { edge, itemSize } = this.props;
-        // let top = pageY - this._locationY;
-        // let left = pageX - this._locationX;
-        const { line, diff, target, boundary } = calLine(
-          ...getLastestTwoValue(this.pointsBuffer),
-          { edge, itemSize },
-        );
-        const start = this.state[target]._value;
-        console.warn(
-          { diff, target, distance: Math.abs(boundary - start), line, start },
-          this.state[target]._value,
-        );
+        // this._locationX = null;
+        // this._locationY = null;
+        // const { nativeEvent } = evt;
+        // // const { pageX, pageY } = nativeEvent;
+        // const { edge, itemSize } = this.props;
+        // // let top = pageY - this._locationY;
+        // // let left = pageX - this._locationX;
+        // const { line, diff, target, boundary } = calLine(
+        //   ...getLastestTwoValue(this.pointsBuffer),
+        //   { edge, itemSize },
+        // );
+        // const start = this.state[target]._value;
+        // console.warn(
+        //   { diff, target, distance: Math.abs(boundary - start), line, start },
+        //   this.state[target]._value,
+        // );
         // this._predictPath({
         //   diff,
         //   target,
@@ -274,55 +274,53 @@ export default function createMoveableComp(Comp) {
         //   line,
         //   start,
         // });
-        return;
-        // const { edge, itemSize } = this.props;
-        // const { left, top } = this.state;
-        // const FINAL_TOP_TOP = edge;
-        // const FINAL_TOP_BOTTOM =
-        //   SCREEN_HEIGHT - TAB_BAR_HEIGHT - edge - itemSize;
-        // const FINAL_LEFT_LEFT = edge;
-        // const FINAL_LEFT_RIGHT = SCREEN_WIDTH - edge - itemSize;
-        // const centerX = SCREEN_WIDTH / 2 - itemSize / 2;
-        // const centerY = SCREEN_HEIGHT / 2 - itemSize / 2;
-        // const right = SCREEN_WIDTH - left._value - itemSize;
-        // const bottom = SCREEN_HEIGHT - top._value - itemSize;
-        // let animation;
-        // const DIFF_TOP = Math.abs(top._value - this._startTop);
-        // const DIFF_LEFT = Math.abs(left._value - this._startLeft);
-        // const DIFF = Math.abs(DIFF_TOP, DIFF_LEFT);
-        // if (DIFF < SCREEN_WIDTH / 3) {
-        //   const MIN = Math.min(top._value, left._value, bottom, right);
-        //   if (MIN === top._value) {
-        //     animation = this._topAnimation(FINAL_TOP_TOP);
-        //   }
-        //   if (MIN === left._value) {
-        //     animation = this._leftAnimation(FINAL_LEFT_LEFT);
-        //   }
-        //   if (MIN === bottom) {
-        //     animation = this._topAnimation(FINAL_TOP_BOTTOM);
-        //   }
-        //   if (MIN === right) {
-        //     animation = this._leftAnimation(FINAL_LEFT_RIGHT);
-        //   }
-        //   setTimeout(animation, 0);
-        //   // requestAnimationFrame(animation);
-        //   return;
-        // }
-        // if (DIFF_TOP > DIFF_LEFT) {
-        //   if (top._value < centerY) {
-        //     animation = this._topAnimation(FINAL_TOP_TOP);
-        //   } else {
-        //     animation = this._topAnimation(FINAL_TOP_BOTTOM);
-        //   }
-        // } else {
-        //   if (left._value < centerX) {
-        //     animation = this._leftAnimation(FINAL_LEFT_LEFT);
-        //   } else {
-        //     animation = this._leftAnimation(FINAL_LEFT_RIGHT);
-        //   }
-        // }
-        // setTimeout(animation, 0);
-        // requestAnimationFrame(animation);
+        // return;
+        const { edge, itemSize } = this.props;
+        const { left, top } = this.state;
+        const FINAL_TOP_TOP = edge;
+        const FINAL_TOP_BOTTOM =
+          SCREEN_HEIGHT - TAB_BAR_HEIGHT - edge - itemSize;
+        const FINAL_LEFT_LEFT = edge;
+        const FINAL_LEFT_RIGHT = SCREEN_WIDTH - edge - itemSize;
+        const centerX = SCREEN_WIDTH / 2 - itemSize / 2;
+        const centerY = SCREEN_HEIGHT / 2 - itemSize / 2;
+        const right = SCREEN_WIDTH - left._value - itemSize;
+        const bottom = SCREEN_HEIGHT - top._value - itemSize;
+        let animation;
+        const DIFF_TOP = Math.abs(top._value - this._startTop);
+        const DIFF_LEFT = Math.abs(left._value - this._startLeft);
+        const DIFF = Math.abs(DIFF_TOP, DIFF_LEFT);
+        if (DIFF < SCREEN_WIDTH / 3) {
+          const MIN = Math.min(top._value, left._value, bottom, right);
+          if (MIN === top._value) {
+            animation = this._topAnimation(FINAL_TOP_TOP);
+          }
+          if (MIN === left._value) {
+            animation = this._leftAnimation(FINAL_LEFT_LEFT);
+          }
+          if (MIN === bottom) {
+            animation = this._topAnimation(FINAL_TOP_BOTTOM);
+          }
+          if (MIN === right) {
+            animation = this._leftAnimation(FINAL_LEFT_RIGHT);
+          }
+          animation()
+          return;
+        }
+        if (DIFF_TOP > DIFF_LEFT) {
+          if (top._value < centerY) {
+            animation = this._topAnimation(FINAL_TOP_TOP);
+          } else {
+            animation = this._topAnimation(FINAL_TOP_BOTTOM);
+          }
+        } else {
+          if (left._value < centerX) {
+            animation = this._leftAnimation(FINAL_LEFT_LEFT);
+          } else {
+            animation = this._leftAnimation(FINAL_LEFT_RIGHT);
+          }
+        }
+        animation()
       },
       onPanResponderTerminationRequest: (evt, gestureState) => true,
 
