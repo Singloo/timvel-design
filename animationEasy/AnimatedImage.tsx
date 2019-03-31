24b93b8d27a6fb70f1b1de17696a892cfc28392d
Singloo <singloo.oo.yang@gmail.com>
2019-03-31 15:44:29 +0800

    fix

--- animationEasy/AnimatedImage.tsx
+++ animationEasy/AnimatedImage.tsx
@@ -4,13 +4,15 @@
  * Created Date: Saturday March 30th 2019
  * Author: Rick yang tongxue(🍔🍔) (origami@timvel.com)
  * -----
- * Last Modified: Saturday March 30th 2019 11:35:41 am
+ * Last Modified: Sunday March 31st 2019 3:44:11 pm
  * Modified By: Rick yang tongxue(🍔🍔) (origami@timvel.com)
  * -----
  */
 import * as React from 'react';
 import { Animated, ImageStyle, ImageProps } from 'react-native';
 import { NAV_BAR_HEIGHT_FULL } from '../utils';
+import FastImage from 'react-native-fast-image';
+const AnimatedFastImage = Animated.createAnimatedComponent(FastImage);
 class AnimatedImage extends React.PureComponent<IProps> {
   static defaultProps: {
     translateY: true;
@@ -27,22 +29,35 @@ class AnimatedImage extends React.PureComponent<IProps> {
   }
 
   _renderChildren = () => {
-    const { scrollY, height, style, translateY, ...restProps } = this.props;
+    const {
+      scrollY,
+      height,
+      style,
+      translateY: showTranslateY,
+      parallaxRatio = 0.5,
+      ...restProps
+    } = this.props;
+    const SCROLL_DISTANCE = height - NAV_BAR_HEIGHT_FULL;
     const scale = this.props.scrollY.interpolate({
       inputRange: [-25, 0],
       outputRange: [1.1, 1],
       extrapolateRight: 'clamp',
     });
     const transform: any[] = [{ scale }];
-    if (translateY) {
+    if (showTranslateY) {
       const translateY = this.props.scrollY.interpolate({
-        inputRange: [-25, 0, height - NAV_BAR_HEIGHT_FULL],
-        outputRange: [-18, 0, -(height - NAV_BAR_HEIGHT_FULL - 40)],
-        extrapolateRight: 'clamp',
+        inputRange: [
+          -25,
+          0,
+          SCROLL_DISTANCE / parallaxRatio,
+          SCROLL_DISTANCE / parallaxRatio + 1,
+        ],
+        outputRange: [-13, 0, SCROLL_DISTANCE, SCROLL_DISTANCE + 1],
+        // extrapolateRight: 'clamp',
       });
       transform.push({ translateY });
     }
-    return <Animated.Image style={[{ transform }, style]} {...restProps} />;
+    return <AnimatedFastImage style={[{ transform }, style]} {...restProps} />;
   };
 }
 
@@ -51,6 +66,7 @@ interface IProps extends ImageProps {
   height: number;
   style: ImageStyle;
   translateY?: boolean;
+  parallaxRatio?: number;
 }
 
 export default AnimatedImage;
